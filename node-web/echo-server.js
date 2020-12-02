d53a7d95a1550c4c8cc915f59ce9f23c7a8f1dcf
Eb Peña <eberg.pena@gmail.com>
2020-12-02 00:15:15 -0600

    Hacer pruebas con POSTMAN

---
+++ node-web/echo-server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const server = http.createServer();
+
+server.on('request',(req, res) => {
+    if(req.mehod === 'POST' && req.url == "/echo"){
+
+    //res.statusCode = 200; 
+    //res.setHeader("Content-Type", "text/plain");
+    res.writeHead(200, {'Content_type': 'text/plain'})
+    res.end('hello world\n');
+    }else{
+        res.statusCode = 404;
+        res.end();
+    }
+   
+    
+});
+
+server.listen(8001);
+console.log("Servidor en la url http://localhost:8001");
